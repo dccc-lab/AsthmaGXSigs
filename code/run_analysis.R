10594a366f25b0ae59a57496e3127d144bd8330e
Damien Croteau-Chonka <damien.croteau-chonka@channing.harvard.edu>
2017-09-26 15:53:45 -0400

    Turned on verbosity during analysis run

--- code/run_analysis.R
+++ code/run_analysis.R
@@ -2,6 +2,7 @@ library("ezknitr")
 library("here")
 
 ezspin(file = here("code", "visualization", "figure01.R"),
-     out_dir = here("reports", "publication"))
+       out_dir = here("reports", "publication"), 
+       verbose = TRUE)
 
 save.image(file = here("data", "final", paste0("GSEA_Run_", rundate_gsea,".rda" )))
